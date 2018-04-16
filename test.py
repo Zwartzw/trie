3356b040d0fb690b5718e1bddf419c0e6a8ca87b
txgyy <tx2gyy@gmail.com>
2018-04-16 20:10:41 +0800

    添加注释

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
     # marisa_trie 举例
     start = time.time()
     with open('data\qq.txt', 'r') as f:
-        trie = marisa_trie.Trie([line.strip() for line in f])
+        trie = marisa_trie.Trie([line.strip() for line in f])   # 只可以一次性导入，但是快
     trie.save(r'data/trie.marisa')
     trie = marisa_trie.Trie()
     trie.load(r'data/trie.marisa')
@@ -21,7 +21,7 @@ if __name__ == '__main__':
     trie = datrie.Trie(string.digits)
     with open('data\qq.txt', 'r') as f:
         for line in f:
-            trie[line.strip()] = True
+            trie[line.strip()] = True                           # 可以多次导入，但是超级慢
     trie.save(r'data/trie.datrie')
     trie = datrie.Trie.load(r'data/trie.datrie')
     print('86269383' in trie)
